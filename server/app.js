b4ec0bb52462c436d4090b7f798fd84e99d3c9a8
Joabe Costa <joabetc@hotmail.com>
2018-03-24 20:04:27 -0300

    Creating an end point for listing sports

--- server/app.js
+++ server/app.js
@@ -5,4 +5,8 @@ let app = express();
 
 app.use(express.static(__dirname + "/../client"));
 
+app.get("/sports", (request, response) => {
+    response.json(["Weightlifting", "Cycling"]);
+});
+
 app.listen(8181, () => console.log("Listening on 8181"));
