cb1ed078e537d41856dffe89026954f6074a058c
Joabe Costa <joabetc@hotmail.com>
2018-03-24 20:02:45 -0300

    Creating the server app

---
+++ server/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let express = require("express");
+let app = express();
+
+app.use(express.static(__dirname + "/../client"));
+
+app.listen(8181, () => console.log("Listening on 8181"));
