2d12c35094a89b26e5618e7b627afef7cfc10d8b
Joabe Costa <joabetc@hotmail.com>
2018-03-30 13:24:54 -0300

    Adding routes by using ui.router

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,16 @@
 import angular from 'angular';
+import '@uirouter/angularjs';
 
-angular.module('olympics', [])
+angular.module('olympics', ['ui.router'])
+    .config(($stateProvider, $urlRouterProvider) => {
+        $urlRouterProvider.otherwise('/sports');
+
+        $stateProvider
+            .state('sports', {
+                url: '/sports',
+                templateUrl: 'sports/sports-nav.html'
+            })
+    })
     .controller('sportsController', function($http) {
         $http.get('/sports').then((response) => {
             this.sports = response.data;
